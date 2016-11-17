1719a278eaaa6f22ab269e009fa3b56e573a4cb0
Amit Bansal <amitbansal26@gmail.com>
2016-11-17 23:20:21 +0530

    updated

--- src/main/java/com/aviation/component/ui/rest/client/AviationComponentApiClient.java
+++ src/main/java/com/aviation/component/ui/rest/client/AviationComponentApiClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.aviation.component.ui.vo.ComponentReport;
 
-@FeignClient(name="aviation-component-api", url="http://localhost:8082")
+@FeignClient("aviation-component-api")
 public interface AviationComponentApiClient {
 	
 	
